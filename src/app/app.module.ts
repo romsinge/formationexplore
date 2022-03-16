81ad5c875d49c4615071519527037433e0b995dd
rsignes <romain.signes@gmail.com>
2022-03-16 16:08:10 +0100

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -32,6 +32,7 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialComponentsModule,
     AppRoutingModule,
--- src/app/components/product-create/product-create.component.ts
+++ src/app/components/product-create/product-create.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { ProductData } from 'src/app/models/product-data.interface';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'explore-product-create',
   templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.scss']
+  styleUrls: ['./product-create.component.scss'],
 })
 export class ProductCreateComponent implements OnInit {
+  productDataForm: FormGroup = new FormGroup({
+    name: new FormControl(
+      '',
+      [Validators.required],
+      [this.isNameAvailable.bind(this)]
+    ),
+    description: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required, this.imageExtension]),
+    price: new FormControl(0, [Validators.required, Validators.min(1)]),
+  });
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleSubmit() {
+    console.log(this.productDataForm);
   }
 
+  imageExtension(control: AbstractControl): ValidationErrors | null {
+    return new RegExp(/^.+\.jpg$/gi).test(control.value)
+      ? null
+      : {
+          imageExtension: true,
+        };
+  }
+
+  // Creer un validateur asynchrone
+  // qui verifie si aucun autre article n'a le meme name
+  isNameAvailable(
+    control: AbstractControl
+  ): Observable<ValidationErrors | null> {
+    return this.productService.productDatas.pipe(
+      map((productDatas) => {
+        return productDatas.some((product) => product.name === control.value)
+          ? {
+              nameNotAvailable: true,
+            }
+          : null;
+      })
+    );
+  }
 }
